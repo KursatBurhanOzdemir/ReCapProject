885333e5c5d8b0c13e28a000755165e6b548c5cf
Kursat Burhan Ozdemir <kursat82697@ogr.duzce.edu.tr>
2021-08-25 19:31:42 +0300

    Proje dosyası ekle.

---
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -0,0 +1,57 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarDal : ICarDal
+    {
+        public void Add(Car entity)
+        {
+            using (ReCapContext context = new ReCapContext())
+            {
+                
+                var addedEntity = context.Entry(entity);
+                addedEntity.State = EntityState.Added;
+                context.SaveChanges();
+
+            }
+        }
+
+        public void Delete(Car entity)
+        {
+            using (ReCapContext context = new ReCapContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Deleted;
+                context.SaveChanges();
+
+            }
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Car entity)
+        {
+            using (ReCapContext context = new ReCapContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = EntityState.Modified;
+                context.SaveChanges();
+
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,64 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _Cars; 
+        public InMemoryCarDal()
+        {
+            _Cars = new List<Car>
+
+            {new Car {Id=1, BrandId=1, ColorId=1 , ModelYear=2005 , DailyPrice= 200 , Description= "hatasız günlük"}
+
+
+            };
+        }
+        public void Add(Car car)
+        {
+            _Cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _Cars.SingleOrDefault(c => c.Id == car.Id);
+            _Cars.Remove(carToDelete);
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAll()
+        {
+            return _Cars;
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAllById(int ıd)
+        {
+            return _Cars.Where(c => c.Id == ıd).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _Cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+           
+        }
+    }
+}
